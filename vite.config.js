0ce81eebf6a27ed8406f9538f561533ad5e284b9
sriram-doradla <d.sriram678@gmail.com>
2024-11-10 21:54:05 +0530

    Updated components and configuration files

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   // Set the base URL for GitHub Pages deployment
-  base: "/app/", // Make sure this matches your GitHub repository name
+  base: '/app/', // Make sure this matches your GitHub repository name
 
   plugins: [react()], // Use the React plugin
 
