2b13e62045908b2dd2a74ec47f72e4760d35f482
sriram-doradla <d.sriram678@gmail.com>
2024-11-11 15:27:35 +0530

    Update Vite config for Render deployment and SPA routing

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // Use the root for Render deployment
+  base: 'https://app-bzcv.onrender.com', // Use the root for Render deployment
 
   plugins: [react()],
 
