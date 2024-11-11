3b6f10564e3cc4c329a0ac9fb05f8dcc60f5ecaa
sriram-doradla <d.sriram678@gmail.com>
2024-11-11 12:15:00 +0530

    Update Vite config for Render deployment

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Set the base URL for GitHub Pages deployment
-  base: '/app/', // Make sure this matches your GitHub repository name
+  base: '/', // Set to root for Render deployment
 
   plugins: [react()], // Use the React plugin
 
